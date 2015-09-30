9a3529b5eaafe4766da5262f62c0972acbddfa03
Chewie <chwthewke@gmail.com>
2015-09-30 08:33:04 +0200

    [ch-calc] Bignum formatter

--- src/main/scala/net/chwthewke/chcalc/package.scala
+++ src/main/scala/net/chwthewke/chcalc/package.scala
@@ -1,5 +1,7 @@
 package net.chwthewke
 
+import scala.collection.immutable.SortedMap
+
 package object chcalc {
   type Seq[T] = scala.collection.immutable.Seq[T]
   val Seq = scala.collection.immutable.Seq
@@ -13,4 +15,21 @@ package object chcalc {
       "D", "!", "@",
       "#", "$", "%",
       "^", "&", "*" )
+
+  implicit class BigIntFormatter( val self : BigInt ) extends AnyVal {
+    def fmtBig : String = {
+
+      val str = self.toString
+      val triples = str.length / 3 + 1
+      val removableTriples = triples - 2
+
+      if ( triples <= 2 )
+        str
+      else if ( removableTriples <= Suffixes.length )
+        str.substring( 0, str.length - 3 * removableTriples ) + Suffixes( removableTriples - 1 )
+      else
+        f"${self.doubleValue}%.3e".filter( _ != '+' )
+    }
+  }
+
 }
